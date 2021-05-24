205b1f04d5d6c25648ae0e7505ba06164b53213f
jinook929 <jinook929@gmail.com>
2021-05-23 22:07:42 -0400

    commit tree b1dbc56fec792df56d3da9c4dfb3a41ca6ea6baa

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+  if(action.type === "ADD_FRIEND") {
+    // let newFriends = {...state}
+    // newFriends.friends.push(action.friend)
+    // return newFriends
+    return {friends: [...state.friends, action.friend]}
+  } else if(action.type === "REMOVE_FRIEND") {
+    return {friends: state.friends.filter(friend => friend.id !== action.id)}
+  } else {
+    return state
+  }
 }
